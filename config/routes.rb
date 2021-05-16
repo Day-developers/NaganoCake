e1f5901556dde9f9958905ecb15335e3eb7ff786
tmy628 <tmy6h1r@gmail.com>
2021-05-16 02:46:42 +0000

    [add] routes_all

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    root to: "homes#top"
+    resources :customers,only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update] do
+      resources :order_items, only: [:update]
+    end
+    resources :items, only: [:new, :create, :index, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
+
+  scope module: :public do
+    root to: "homes#top"
+    get "homes/about" => "homes#about", as: "about"
+    resource :customers, only: [:show, :edit, :update, :unsubscribe, :withdraw]
+    resources :orders, only: [:new, :confirm, :create, :complete, :index, :show]
+    resources :items, only: [:index, :show]
+    resources :cart_items, only: [:index, :create, :update, :destroy, :destroy_all]
+    resources :addresses, only: [:index, :create, :destroy, :edit, :update]
+  end
 end
