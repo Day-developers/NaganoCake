3980b45aa5bbba4a8609b375d8e12de21c837163
papi-gorilla <jindeensadayoshi@gmail.com>
2021-05-16 09:14:33 +0000

    [add] ログイン

--- db/migrate/20210516014656_devise_create_admins.rb
+++ db/migrate/20210516014656_devise_create_admins.rb
@@ -4,14 +4,17 @@ class DeviseCreateAdmins < ActiveRecord::Migration[5.2]
   def change
     create_table :admins do |t|
       ## Database authenticatable
+
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
+
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
 
       ## Rememberable
+
       t.datetime :remember_created_at
 
       ## Trackable
@@ -38,7 +41,8 @@ class DeviseCreateAdmins < ActiveRecord::Migration[5.2]
 
     add_index :admins, :email,                unique: true
     add_index :admins, :reset_password_token, unique: true
-    # add_index :admins, :confirmation_token,   unique: true
-    # add_index :admins, :unlock_token,         unique: true
+
+    add_index :admins, :confirmation_token,   unique: true
+    add_index :admins, :unlock_token,         unique: true
   end
 end
