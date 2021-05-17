b955215beca9ddc169b46840e71113278a3c5148
RIKO-tlov <bluebyrair@outlook.jp>
2021-05-17 14:07:23 +0000

    [add]カート機能/一覧

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,7 +2,7 @@ class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
 
-  def  add_tax_sales_price
-    (self.sales_price * 1.10).round
+  def  add_tax_price
+    (self.price * 1.10).round
   end
 end
