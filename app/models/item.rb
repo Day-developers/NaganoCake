887f179a95321a16f21c8c6d9a079160d3a7f663
tmy628 <tmy6h1r@gmail.com>
2021-05-18 01:54:58 +0000

    [add] 管理者 商品新規登録・一覧・詳細・編集・情報更新機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
-  attachment :image
+    belongs_to :genre
+    has_many :cart_items, dependent: :destroy
+    has_many :order_items, dependent: :destroy
+
+    attachment :image
+
+    validates :genre_id, :name, :price, presence: true
+    validates :caption, length: {maximum: 200}
+    validates :price, numericality: { only_integer: true }
+
 end
