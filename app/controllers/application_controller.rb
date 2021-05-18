6e70f49dba04f820b18d25ac757a59c58222aac2
papi-gorilla <jindeensadayoshi@gmail.com>
2021-05-18 02:43:50 +0000

    [add] ログインページのレイアウト、ログイン後のリダイレクト先

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    if resource == :admin
+    case resource
+    when Admin
       admin_root_path
-    else
+    when Customer
       root_path
     end
   end
