f3fdf67f44256894110bee7e18d156ca07f92589
RIKO-tlov <bluebyrair@outlook.jp>
2021-05-18 13:52:15 +0000

    [fix]カート削除機能、一覧表示

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,7 +2,7 @@ class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
 
-  def  add_tax_price
-    (self.price * 1.10).round
+  def  amount
+    (cart_item.item.price*1.10*cart_item.number).round
   end
 end
