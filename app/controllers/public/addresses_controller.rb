61903e04d1ea6b32f994f23c418e1b17dbad5262
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-05-18 09:43:43 +0000

    [add]配送先追加・編集機能

---
+++ app/controllers/public/addresses_controller.rb
@@ -0,0 +1,44 @@
+class Public::AddressesController < ApplicationController
+  before_action :authenticate_customer!
+  
+  def index
+    @address = Address.new
+    @addresses = current_customer.addresses
+  end
+  
+  def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    @addresses = current_customer.addresses
+    if @address.save
+      redirect_to addresses_path
+    else
+      render 'index'
+    end
+  end
+  
+  def edit
+    @address = Address.find(params[:id])
+  end
+  
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path(@address)
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
+  end
+  
+  private
+  
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
+  end
+end
