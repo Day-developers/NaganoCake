e864f5d094a9c072c28fcc0a8d328cb278d056b4
tmy628 <tmy6h1r@gmail.com>
2021-05-19 04:14:15 +0000

    [add] 注文情報の確認画面を表示

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,20 +3,25 @@ class Public::OrdersController < ApplicationController
 
   def new
     @order = Order.new
-  end
-
-  def confirm
-  end
-
-  def create
-  end
-
-  def complete
+    @addresses = Address.all
   end
 
   def index
   end
 
   def show
+  end
+
+  def confirm
+    @cart_items = current_customer.cart_items
+    @order = Order.new
+  end
+
+  def create
+    order = Order.new(order_params)
+    order.save
+  end
+
+  def complete
   end
 end
