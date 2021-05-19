47932d021df98a91b18f11e319303f43a3dc9a54
tmy628 <tmy6h1r@gmail.com>
2021-05-19 11:23:23 +0000

    注文情報の確認画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,12 +3,30 @@ class Public::OrdersController < ApplicationController
 
   def new
     @order = Order.new
-    @addresses = Address.all
+    @addresses = Address.where(customer: current_customer)
   end
 
   def confirm
     @cart_items = current_customer.cart_items
-    @order = Order.new
+    @order = Order.new(order_params)
+
+    if params[:order][:addresses] == "address"
+      @order.postal_code = current_customer.postal_code
+      @order.address     = current_customer.address
+      @order.name        = current_customer.last_name + current_customer.first_name
+
+    elsif params[:order][:addresses] == "addresses"
+      addresses = Addresses.find(params[:order][:address_id])
+      @order.postal_code = addresses.postal_code
+      @order.address     = addresses.address
+      @order.name        = addresses.name
+
+    elsif params[:order][:addresses] == "new_address"
+      @order.postal_code = params[:order][:postal_code]
+      @order.address     = params[:order][:address]
+      @order.name        = params[:order][:name]
+      @addresses = "1"
+    end
   end
 
   def create
@@ -25,5 +43,13 @@ class Public::OrdersController < ApplicationController
 
   def show
     @order=Order.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :shipping_fee, :price, :status)
+  end
+
+  def addresses_params
+    params.require(:order).permit(:postal_code, :address, :name)
   end
 end
