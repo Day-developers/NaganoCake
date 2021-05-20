77e583e33710053f82f6493ed2d909e44ea5dc5c
papi-gorilla <jindeensadayoshi@gmail.com>
2021-05-20 12:52:56 +0000

    [add/fix] 管理者の注文履歴詳細機能、顧客の注文詳細

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,19 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order=Order.find(params[:id])
+    @customer=Customer.find_by(id: @order.customer_id)
+    @order_item=OrderItem.where(order_id: @order.id)
   end
+
+  def update
+    @order=Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to admin_order_path(@order.id)
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:status)
+  end
+
 end
