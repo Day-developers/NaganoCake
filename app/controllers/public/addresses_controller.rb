be5e7eda5f4aed0cdc57a8aacf94b1d1d75c30e6
tmy628 <tmy6h1r@gmail.com>
2021-05-22 14:30:13 +0000

    [add] 配送先 新規登録・削除 非同期

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,27 +1,28 @@
 class Public::AddressesController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def index
     @address = Address.new
     @addresses = current_customer.addresses
   end
-  
+
   def create
+    @addresses = Address.all
     @address = Address.new(address_params)
     @address.customer_id = current_customer.id
     @addresses = current_customer.addresses
     if @address.save
       flash[:success] = "配送先を追加しました"
-      redirect_to addresses_path
+      # redirect_to addresses_path
     else
       render 'index'
     end
   end
-  
+
   def edit
     @address = Address.find(params[:id])
   end
-  
+
   def update
     @address = Address.find(params[:id])
     if @address.update(address_params)
@@ -31,16 +32,17 @@ class Public::AddressesController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
+    @addresses = Address.all
     @address = Address.find(params[:id])
     @address.destroy
     flash[:success] = "配送先を削除しました"
-    redirect_to addresses_path
+    # redirect_to addresses_path
   end
-  
+
   private
-  
+
   def address_params
     params.require(:address).permit(:postal_code, :address, :name)
   end
