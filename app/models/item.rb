f76c163eca1454c72dec3605fda480392d1c2490
RIKO-tlov <bluebyrair@outlook.jp>
2021-05-23 03:51:09 +0000

    [add]管理者：会員・商品検索

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,14 @@ class Item < ApplicationRecord
     validates :genre_id, :name, :price, presence: true
     validates :caption, length: {maximum: 200}
     validates :price, numericality: { only_integer: true }
+
+    def self.looks(search)
+      return none if search.blank?
+      if search
+        Item.where(['name LIKE ?', "%#{search}%"])
+      else
+        Item.all
+      end
+    end
+
 end
